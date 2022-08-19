8a197b12e400c813e44f279c072b6a6b811153eb
Maycon da Silva Nunes <58365472+maycondsn@users.noreply.github.com>
2022-08-18 23:28:23 -0400

    iniciando conexão entre o servidor e o cliente

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 import socket
 
+# Definindo o servidor e a porta
 HOST = '127.0.0.1'  # localhost
 PORT = 3333
 
+# Iniciando um objeto socket
 client = socket.socket()
 
-client.connect((HOST, PORT))
+# Tentando se connectar ao servidor
+try:
+    client.connect((HOST, PORT))
+except socket.error as e:
+    print(str(e))
 
 print(f'client online in {HOST}:{PORT}')
+
+# Loop para aguardar uma mensagem do servidor
+while True:
+    message = client.recv(2048).decode('utf-8')
+    print(message)
--- server.py
+++ server.py
@@ -1,15 +1,27 @@
 import socket
+from _thread import *
 
+# Definindo o servidor e a porta para a conexão
 HOST = '127.0.0.1'
 PORT = 3333
 
+# Iniciando um objeto socket
 server = socket.socket()
 
-server.bind((HOST, PORT))
+# Vincular o objeto ao endereço
+try:
+    server.bind((HOST, PORT))
+except socket.error as e:
+    print(str(e))
 
+# Botar o servidor em modo de escuta, aguardando uma conexão
 print(f'Server online in {HOST}:{PORT}')
 server.listen()
 
+# loop para aceitar as conexões
 while True:
     client, addres = server.accept()
-    print(f'Connected to {addres}')
+    print(f'Connected to {addres[0]}:{addres[1]}')
+
+    # envia uma mensagem ao cliente conectado
+    client.send(str.encode(str(f'Connected to server!')))
