db86fd26088db7eaacc6bd465c120d3a6768e73a
Maycon da Silva Nunes <58365472+maycondsn@users.noreply.github.com>
2022-08-20 10:37:07 -0400

    enviando mensagens ao servidor

--- main.py
+++ main.py
@@ -1,11 +1,43 @@
 import socket
+from _thread import *
+import time
 
 # Definindo o servidor e a porta
 HOST = '127.0.0.1'  # localhost
-PORT = 3333
+PORT = 33333
 
 # Iniciando um objeto socket
 client = socket.socket()
+
+
+# aguardar mensagem do servidor
+def listen_for_message_from_server(client):
+    while True:
+        message = client.recv(2048).decode('utf-8')
+        print(message)
+
+
+# enviar mensagens ao servidor
+def send_messages_to_server(client):
+    time.sleep(2)
+    username = input('enter your username: ')
+    while True:
+        message = input('\nmessage: ')
+
+        if (message == ''):
+            print('empty message')
+            exit(0)
+
+        else:
+            response = f'{username}: {message}'
+            client.sendall(response.encode())
+
+
+def communicate_to_server(client):
+    # dividir o processo
+    start_new_thread(listen_for_message_from_server, (client, ))
+    send_messages_to_server(client)
+
 
 # Tentando se connectar ao servidor
 try:
@@ -15,7 +47,4 @@ except socket.error as e:
 
 print(f'client online in {HOST}:{PORT}')
 
-# Loop para aguardar uma mensagem do servidor
-while True:
-    message = client.recv(2048).decode('utf-8')
-    print(message)
+communicate_to_server(client)
--- server.py
+++ server.py
@@ -3,7 +3,7 @@ from _thread import *
 
 # Definindo o servidor e a porta para a conexão
 HOST = '127.0.0.1'
-PORT = 3333
+PORT = 33333
 
 # Iniciando um objeto socket
 server = socket.socket()
@@ -18,10 +18,14 @@ except socket.error as e:
 print(f'Server online in {HOST}:{PORT}')
 server.listen()
 
+
+def thread_client(client):
+    # envia uma mensagem ao cliente conectado
+    client.send(str.encode(str(f'Connected!')))
+
+
 # loop para aceitar as conexões
 while True:
     client, addres = server.accept()
     print(f'Connected to {addres[0]}:{addres[1]}')
-
-    # envia uma mensagem ao cliente conectado
-    client.send(str.encode(str(f'Connected to server!')))
+    start_new_thread(thread_client, (client, ))
